93ea7652420f425bc32ab7ee09c373af078c62cf
Kevin Boston <kbos22@gmail.com>
2020-07-16 11:07:30 +0000

    Automatically backed up by Learn

--- db/migrate/008_add_genre_and_season_to_show.rb
+++ db/migrate/008_add_genre_and_season_to_show.rb
@@ -1,6 +1,6 @@
 class AddGenreAndSeasonToShow < ActiveRecord::Migration[5.1]
   def change
-    add_column :genre, :shows, :string
-    add_column :season, :shows, :string
+    add_column :shows, :genre, :string
+    add_column :shows, :season, :string
   end
 end
