0f3f5481dea73602a868e95314a378cff527922d
Kevin Boston <kbos22@gmail.com>
2020-07-16 11:10:04 +0000

    Automatically backed up by Learn

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -6,6 +6,6 @@ class Actor < ActiveRecord::Base
     "#{self.first_name} #{self.last_name}"
   end
   def list_roles
-    self.characters.collect {|char|"#{char.name} - #{char.show}"}
+    characters.collect {|char|"#{char.name} - #{char.show}"}
   end
 end
